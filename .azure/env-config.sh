3e4bba3a504869bf798fdf0a4c237aea8a1e128d
andremarko <andre.marcolongo@gmail.com>
2025-10-06 19:24:48 -0300

    feat(azure): adiciona scripts para deploy gh actions - azure e refatora applications.properties

---
+++ .azure/env-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export RESOURCE_GROUP="rg-cashflow"
+export SERVER_NAME="cashflow-sqlserver"
+export DB_ADMIN="cashflow-admin"
+export DB_NAME="cashflow-backend-entities"
+export DB_PASSWORD="FluxoCaixa@2025"
+
+export WEBAPP_NAME="cashflow-mvc"
+export APP_SERVICE_PLAN="planCashFlow"
+
+export LOCATION="westus"
+export RUNTIME="JAVA:21-java21"
+export GITHUB_REPO="andremarko/ads-cp5-devops-4sem"
+export BRANCH="main"
+
+export MY_PUBLIC_IP=$(curl -s ifconfig.me)
+
+export JDBC_CONNECTION_STRING=$(az sql db show-connection-string --client jdbc --name $DB_NAME --server $SERVER_NAME --output tsv)
