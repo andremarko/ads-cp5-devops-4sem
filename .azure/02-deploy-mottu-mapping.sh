9d05b18e21f94e3a4bee714b25a030ad0ad95b48
andremarko <151890840+andremarko@users.noreply.github.com>
2025-10-07 00:31:43 -0300

    refactor variavel nome do resource group

--- .azure/02-deploy-mottu-mapping.sh
+++ .azure/02-deploy-mottu-mapping.sh
@@ -4,7 +4,7 @@ source ./env-config.sh
 
 az monitor app-insights component create --app $APP_INSIGHTS_NAME --location $LOCATION --resource-group $RESOURCE_GROUP --application-type web
 
-export APP_INSIGHTS_CONNECTION_STRING=$(az monitor app-insights component show --app "$APP_INSIGHTS_NAME" --resource-group "$RESOURCE_GROUP_NAME" --query connectionString --output tsv)
+export APP_INSIGHTS_CONNECTION_STRING=$(az monitor app-insights component show --app "$APP_INSIGHTS_NAME" --resource-group "$RESOURCE_GROUP" --query connectionString --output tsv)
 
 az appservice plan create --name $APP_SERVICE_PLAN --resource-group $RESOURCE_GROUP --location $LOCATION --sku B1 --is-linux
 
@@ -31,7 +31,7 @@ az webapp restart --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP
 az monitor app-insights component connect-webapp \
   --app "$APP_INSIGHTS_NAME" \
   --web-app "$WEBAPP_NAME" \
-  --resource-group "$RESOURCE_GROUP_NAME"
+  --resource-group "$RESOURCE_GROUP"
 
 az webapp deployment github-actions add --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --repo $GITHUB_REPO --branch $BRANCH --login-with-github
 
